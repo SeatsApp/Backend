5bfd927259d702fefc5ab401def8bd7d72069763
tvdw2000 <thomas.vdwalle@hotmail.com>
2022-04-19 10:27:06 +0200

    Setup Spring Boot

---
+++ src/main/java/com/seatapp/SeatAppApplication.java
@@ -0,0 +1,13 @@
+package com.seatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seatapp/SeatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
