91743f7b050066347cf0d61da003e9275bd05f07
tvdw2000 <thomas.vdwalle@hotmail.com>
2022-05-24 09:31:51 +0200

    Change table user to app_user

--- adapters/outgoing/jpa/src/main/java/com/seatapp/entities/UserEntity.java
+++ adapters/outgoing/jpa/src/main/java/com/seatapp/entities/UserEntity.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "User")
+@Table(name = "app_user")
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter
 public class UserEntity {
